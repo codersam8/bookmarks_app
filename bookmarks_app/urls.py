cff325391cf97ccd67225ec01484fc7e0565fa81
sampath surineni <vinnu@SSURINEN-IN.oradev.oraclecorp.com>
2018-05-30 08:58:29 +0530

    added login page

--- bookmarks_app/urls.py
+++ bookmarks_app/urls.py
@@ -1,3 +1,4 @@
+
 """bookmarks_app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -14,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import login
 from django.urls import include, path
 
 urlpatterns = [
     path('bookmarks/', include('bookmarks.urls')),
+    path('login/', login),
     path('admin/', admin.site.urls),
 ]
