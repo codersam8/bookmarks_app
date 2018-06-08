7d7beb12e6412718403786244e25f737f818ad3d
sampath <sampath.surineni@oracle.com>
2018-06-08 08:22:22 +0530

    added link to tags in base

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -6,7 +6,7 @@ from .views import main_page, user_page, bookmark_save_page, tag_page, tag_cloud
 urlpatterns = [
     path('', main_page),
     path('save/', bookmark_save_page),
-    path('tag/', tag_cloud_page),
+    path('tags/', tag_cloud_page),
     path('tag/<slug:tag_name>/', tag_page),
     path('user/<str:username>/', user_page)
 ]
