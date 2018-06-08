2c3bd05e455081c1d02bc332017c0669419772b5
sampath <sampath.surineni@oracle.com>
2018-06-08 08:31:29 +0530

    added jquery cdn

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from .views import main_page, user_page, bookmark_save_page, tag_page, tag_cloud_page
+from .views import (main_page, user_page,
+                    bookmark_save_page, tag_page, tag_cloud_page)
 
 
 urlpatterns = [
